2af4c80cad450e0401cec1697c541178f5ac5811
sohyun <sh0000@pusan.ac.kr>
2024-12-14 00:36:07 +0900

    [#18] [Chore] domain으로 변경하는 로직 위치 수정

--- logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hub/framework/web/dto/HubAddressCreateRequest.java
+++ logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hub/framework/web/dto/HubAddressCreateRequest.java
@@ -1,6 +1,5 @@
 package com.sparta.blackyolk.logistic_service.hub.framework.web.dto;
 
-import com.sparta.blackyolk.logistic_service.hub.application.domain.HubForCreate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
@@ -25,23 +24,5 @@ public record HubAddressCreateRequest(
     String zipCode
 ) {
 
-    public static HubForCreate toDomain(
-        Long userId,
-        String role,
-        HubCreateRequest hubCreateRequest
-    ) {
-        return new HubForCreate(
-            userId,
-            role,
-            hubCreateRequest.hubManagerId(),
-            hubCreateRequest.name(),
-            hubCreateRequest.address().sido,
-            hubCreateRequest.address().sigungu,
-            hubCreateRequest.address().eupmyun,
-            hubCreateRequest.address().roadName,
-            hubCreateRequest.address().buildingNumber,
-            hubCreateRequest.address().zipCode
-        );
-    }
 }
 
