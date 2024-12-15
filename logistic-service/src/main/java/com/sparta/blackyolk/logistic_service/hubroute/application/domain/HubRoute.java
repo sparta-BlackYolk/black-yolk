22c8e01d07bb58bfa753883923302d76e4280ec4
sohyun <sh0000@pusan.ac.kr>
2024-12-15 18:02:33 +0900

    [#22] [Chore] HubRoute 엔티티 distance, duration 필드 타입 수정

--- logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hubroute/application/domain/HubRoute.java
+++ logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hubroute/application/domain/HubRoute.java
@@ -3,7 +3,6 @@ package com.sparta.blackyolk.logistic_service.hubroute.application.domain;
 import com.sparta.blackyolk.logistic_service.hub.application.domain.Hub;
 import com.sparta.blackyolk.logistic_service.hubroute.data.vo.HubRouteStatus;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,30 +16,23 @@ public class HubRoute implements Serializable {
     private Hub departureHub;
     private Hub arrivalHub;
     private HubRouteStatus status;
-    private Integer duration;
-    private BigDecimal distance;
+    private Long duration;
+    private Long distance;
 
     public HubRoute(
         Hub departureHub,
-        Hub arrivalHub
+        Hub arrivalHub,
+        Long distance,
+        Long duration
     ) {
         this.departureHub = departureHub;
         this.arrivalHub = arrivalHub;
-        this.distance = calculateDistance();
-        this.duration = calculateDuration();
+        this.distance = distance;
+        this.duration = duration;
     }
 
     public boolean isDepartureHubBelongToHubRoute(String departureHubId) {
         return departureHubId.equals(departureHub.getHubId());
     }
 
-    // TODO : 코드 완성하기 - 미리 계산해놓은 것 반영시키는 코드로 작성하기
-    public BigDecimal calculateDistance() {
-        return new BigDecimal("50.5");
-    }
-
-    // TODO : 코드 완성하기 - 미리 계산해놓은 것 반영시키는 코드로 작성하기
-    public Integer calculateDuration() {
-        return 40;
-    }
 }
