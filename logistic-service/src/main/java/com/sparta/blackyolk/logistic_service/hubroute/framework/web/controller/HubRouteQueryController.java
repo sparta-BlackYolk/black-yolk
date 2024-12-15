87f4ee4da00e7c7fe92b2464fc16a4944417b6a2
sohyun <sh0000@pusan.ac.kr>
2024-12-15 23:39:00 +0900

    [#20] [Add] Controller에 userId 및 role 헤더 추가

--- logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hubroute/framework/web/controller/HubRouteQueryController.java
+++ logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/hubroute/framework/web/controller/HubRouteQueryController.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,10 +30,11 @@ public class HubRouteQueryController {
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{hubRouteId}")
     public HubRouteGetResponse getHubRoute(
+        @RequestHeader(value = "X-User-Id", required = true) String userId,
+        @RequestHeader(value = "X-Role", required = true) String role,
         @PathVariable(value = "hubId") String hubId,
         @PathVariable(value = "hubRouteId") String hubRouteId
     ) {
-        // TODO : user token, user role 받아야 하나?
         HubRouteForRead hubRouteForRead = new HubRouteForRead(
             hubId,
             hubRouteId
@@ -45,11 +47,14 @@ public class HubRouteQueryController {
     @GetMapping
     @PaginationConstraint(defaultSort = "createdAt", defaultDirection = "DESC", availableSize = {10,30,50}, defaultSize = 10)
     public HubRoutePageResponse getHubRoutes(
+        @RequestHeader(value = "X-User-Id", required = true) String userId,
+        @RequestHeader(value = "X-Role", required = true) String role,
         @PathVariable(value = "hubId") String hubId,
         Pageable pageable,
         @RequestParam(required = false) String keyword
     ) {
-        // TODO : user token, user role 받아야 하나?
+        log.info("[HubRoute 목록 조회] userId: {}", userId);
+
         return hubRouteCacheService.getHubRoutes(hubId, pageable, keyword);
     }
 
