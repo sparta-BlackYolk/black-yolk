c0140423efb1b98e78a9a8d28caaff14a99752d6
sohyun <sh0000@pusan.ac.kr>
2024-12-16 19:59:10 +0900

    [#64] [Add] 예외처리 추가

--- logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/common/exception/ErrorCode.java
+++ logistic-service/src/main/java/com/sparta/blackyolk/logistic_service/common/exception/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     // 공통
     UNAUTHORIZED(401, "UNAUTHORIZED", "로그인을 해주세요."),
     FORBIDDEN(403, "USER_FORBIDDEN", "접근 권한이 없습니다."),
+    REQUEST_ACCEPTED(202, "REQUEST_ACCEPTED", "잠시만 기다려주세요."),
+    SERVICE_UNAVAILABLE(503, "SERVICE_UNAVAILABLE", "잠시후에 다시 요청해주세요."),
 
     // Hub
     HUB_BAD_REQUEST(400, "HUB_BAD_REQUEST", "잘못된 허브 요청입니다."),
@@ -41,7 +43,7 @@ public enum ErrorCode {
     RESOURCE_NOT_EXIST(404, "{RESOURCE}_NOT_EXIST", "클라이언트가 요청한 특정 리소스를 찾을 수 없는 경우"),
     RESOURCE_ALREADY_EXIST(409, "{RESOURCE}_ALREADY_EXIST", "클라이언트가 요청한 리소스가 이미 존재하는 경우"),
 
-    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류"),
+    INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버에 일시적인 오류가 발생했습니다."),
     CACHE_CONNECTION_ERROR(500, "CACHE_CONNECTION_ERROR", "캐시 오류");
 
     private final int code;
